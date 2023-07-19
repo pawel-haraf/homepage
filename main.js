6e52d6ad9a692aceda2b86dc59966a00e53cbcf8
Pawel Haraf <haraf.p@gmail.com>
2023-07-19 22:42:02 +0200

    remove ; from js file

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const firstName = 'Paweł Haraf';
-const age = 37;
+const firstName = 'Paweł Haraf'
+const age = 37
 
-console.log(`Cześć :) Nazywam się ${firstName} i mam ${age} lat`);
+console.log(`Cześć :) Nazywam się ${firstName} i mam ${age} lat`)
 
 // moduł 3 DOMowe przedszkole
-const header = document.querySelector('.page-header__heading--js');
-console.log(header);
-console.log(header.innerHTML);
-header.innerHTML = `Cześć :) Nazywam się ${firstName} i mam ${age} lat. To jest tekst nagłówka zmienionego poprzez JS:`;
-console.log(header.style);
+const header = document.querySelector('.page-header__heading--js')
+console.log(header)
+console.log(header.innerHTML)
+header.innerHTML = `Cześć :) Nazywam się ${firstName} i mam ${age} lat. To jest tekst nagłówka zmienionego poprzez JS:`
+console.log(header.style)
 header.style.color = 'red'
