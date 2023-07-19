f7128b8f7cae1055e55aba44e66cba8266388f77
Pawel Haraf <haraf.p@gmail.com>
2023-07-19 23:56:28 +0200

    Added empty element and completed it with JavaScript

--- main.js
+++ main.js
@@ -10,3 +10,8 @@ console.log(header.innerHTML)
 header.innerHTML = `Cześć :) Nazywam się ${firstName} i mam ${age} lat. To jest tekst nagłówka zmienionego poprzez JS:`
 console.log(header.style)
 header.style.color = 'red'
+
+// zadanie domowe z trzeciego tygodnia
+const content = document.querySelector('.article__content--js')
+console.log(content)
+content.innerHTML = `Zawartość paragrafu była pusta. Została uzupełniona poprzez javaScript. Jest to możliwe dzięki temu, że JS ma dostęp do różnych elementów w HTMLu czy CSSie poprzez DOM. Dzięki temu strony mogą być bardziej interaktywne.`
