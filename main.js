74e1294ea9bafe331114d793baa7b0ab8611ac55
Pawel Haraf <haraf.p@gmail.com>
2023-07-19 22:38:06 +0200

    learning about DOM

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const firstName = 'Paweł';
+const firstName = 'Paweł Haraf';
 const age = 37;
 
 console.log(`Cześć :) Nazywam się ${firstName} i mam ${age} lat`);
+
+// moduł 3 DOMowe przedszkole
+const header = document.querySelector('.page-header__heading--js');
+console.log(header);
+console.log(header.innerHTML);
+header.innerHTML = `Cześć :) Nazywam się ${firstName} i mam ${age} lat. To jest tekst nagłówka zmienionego poprzez JS:`;
+console.log(header.style);
+header.style.color = 'red'
