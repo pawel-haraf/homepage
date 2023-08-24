1527a810bc3565662b2801a0dd65d1b4cdbaf80a
Pawel Haraf <haraf.p@gmail.com>
2023-08-24 21:36:25 +0200

    Learning about loops in js

---
+++ src/js/loops.js
@@ -0,0 +1,60 @@
+// WTF 09.04 - pętle
+// pętla for
+for (let i = 0; i < 3; i++) {
+	console.log(`${i} wykonanie pętli`)
+}
+
+let number = 0
+
+console.log(number++)
+console.log(number)
+
+//  Wypiszmy elementy tablicy
+
+const elements = ['😎', '🤯', '😱']
+
+for (let i = 0; i < elements.length; i++) {
+	console.log(`na miejscu ${i} jest ${elements[i]}`)
+}
+
+// pętla while
+
+let n = 3
+while (n < 9) {
+	console.log(n)
+	n = n + 3
+}
+console.log(n)
+
+// pętla do while
+
+let m = 2
+do {
+	console.log(m)
+	m = m + 2
+} while (m < 9)
+console.log(m)
+
+// for in
+const deathStar = {
+	diameter: 120000,
+	fire: target => {
+		console.log(`${target} destroyed 💣`)
+	},
+	isOperating: true,
+	levels: 357,
+	name: 'Death Star',
+	population: 10000,
+	comander: {
+		name: 'darth vader',
+	},
+}
+
+for (property in deathStar) {
+	console.log(property)
+	console.log(`${property}: ${deathStar[property]}`)
+}
+
+for (let element of elements) {
+	console.log(element)
+}
